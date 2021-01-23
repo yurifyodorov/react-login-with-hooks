1927911f4b6995890e7f58acac932fc01d55658c
Yuri Fedorov <y.fedorov@nekor.ru>
2021-01-23 17:41:31 +0300

    card crntered

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import 'rsuite/dist/styles/rsuite-default.css';
+import './App.scss';
 
 import LoginForm from '../LoginForm/LoginForm';
 
 const App: React.FC = () => {
   return (
-    <LoginForm />
+    <div className="App">
+      <LoginForm />
+    </div>
   );
-}
+};
 
 export default App;
